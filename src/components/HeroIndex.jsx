0f293c8cc0d386f94521c810db1aa119f7545f94
Daniela <dare0002@stud.kea.dk>
2025-05-23 23:21:45 +0200

    add state to animations to avoid SSR problems

--- src/components/HeroIndex.jsx
+++ src/components/HeroIndex.jsx
@@ -1,10 +1,16 @@
 "use client";
 
-import Image from "next/image";
+import { useEffect, useState } from "react"; 
 import Button from "./Button";
 import { motion } from "framer-motion";
 
 const Hero = () => {
+  const [showContent, setshowContent] = useState(false); 
+
+  useEffect(() => {
+    setshowContent(true); 
+  }, []);
+
   const handleScroll = () => {
     const section = document.getElementById("materiale");
     if (section) {
@@ -15,47 +21,59 @@ const Hero = () => {
   return (
     <div className="bg-lightgreen text-green w-full py-6 mb-8">
       <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-8 relative">
-        <motion.img
-                    src="/sallysFarKids.svg"
-                    alt="Sallys far og to børn illustration"
-                    className="w-54 scale-125 max-h-[400px] hidden md:block object-contain mx-auto z-0"
-                    initial={{ x: '-100%', opacity: 1 }}
-                    animate={{ x: '40%', opacity: 0 }}
-                    transition={{ duration: 1.1, ease: 'easeOut' }}
-                /> 
-        <motion.img
-                src="/girlDog.svg"
-                alt="Pige med en hund illustration"
-                className="absolute w-53 scale-100 max-h-[250px] hidden md:block object-contain mx-auto z-0"
-                initial={{ x: '-120%', opacity: 0 }}
-                animate={{ x: 0, opacity: 1 }}
-                transition={{ duration: 1.5, ease: 'easeInOut', delay:0.5 }}
-                />
+        {showContent && (
+          <>
+            <motion.img
+              src="/sallysFarKids.svg"
+              alt="Sallys far og to børn illustration"
+              className="w-54 scale-125 max-h-[400px] hidden md:block object-contain mx-auto z-0"
+              initial={{ x: "-100%", opacity: 1 }}
+              animate={{ x: "40%", opacity: 0 }}
+              transition={{ duration: 1.1, ease: "easeOut" }}
+            />
+            <motion.img
+              src="/girlDog.svg"
+              alt="Pige med en hund illustration"
+              className="absolute w-53 scale-100 max-h-[250px] hidden md:block object-contain mx-auto z-0"
+              initial={{ x: "-120%", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1.5, ease: "easeInOut", delay: 0.5 }}
+            />
+          </>
+        )}
 
         <div className="flex flex-col items-center text-center z-10 relative">
           <h1 className="text-4xl md:text-8xl font-bold mb-1">CARLSEN</h1>
           <h4 className="text-2xl md:text-3xl font-semibold">Ekstra</h4>
-          <p className="text-base md:text-lg font-semibold space-y-2">Gratis undervisningsmateriale </p>
-          <p className="text-base md:text-lg font-semibold mb-6">fra Danmarks største børnebogsforlag </p>
+          <p className="text-base md:text-lg font-semibold space-y-2">
+            Gratis undervisningsmateriale
+          </p>
+          <p className="text-base md:text-lg font-semibold mb-6">
+            fra Danmarks største børnebogsforlag
+          </p>
           <Button onClick={handleScroll}>Find materiale nu</Button>
         </div>
 
-         <motion.img
-                src="/girlDog.svg"
-                alt="Pige med en hund illustration"
-                className="absolute left-1/2 transform -translate-x-1/2 w-54 scale-100 max-h-[250px] hidden md:block object-contain mx-auto z-0"
-                initial={{ x: 150, opacity: 1 }}
-                animate={{ x: '120%', opacity: 0 }}
-                transition={{ duration: 0.9, ease: 'easeInOut' }}
-                />
-                <motion.img
-                    src="/sallysFarKids.svg"
-                    alt="Sallys far og to børn illustration"
-                    className="absolute left-1/2 transform -translate-x-1/2 w-54 scale-125 max-h-[400px] hidden md:block object-contain mx-auto z-0"
-                    initial={{ x: '40%', opacity: 0 }}
-                    animate={{ x: 300, opacity: 1 }}
-                    transition={{ duration: 1, ease: 'easeOut', delay: 1.1 }}
-                />
+        {showContent && (
+          <>
+            <motion.img
+              src="/girlDog.svg"
+              alt="Pige med en hund illustration"
+              className="absolute left-1/2 transform -translate-x-1/2 w-54 scale-100 max-h-[250px] hidden md:block object-contain mx-auto z-0"
+              initial={{ x: 150, opacity: 1 }}
+              animate={{ x: "120%", opacity: 0 }}
+              transition={{ duration: 0.9, ease: "easeInOut" }}
+            />
+            <motion.img
+              src="/sallysFarKids.svg"
+              alt="Sallys far og to børn illustration"
+              className="absolute left-1/2 transform -translate-x-1/2 w-54 scale-125 max-h-[400px] hidden md:block object-contain mx-auto z-0"
+              initial={{ x: "40%", opacity: 0 }}
+              animate={{ x: 300, opacity: 1 }}
+              transition={{ duration: 1, ease: "easeOut", delay: 1.1 }}
+            />
+          </>
+        )}
       </div>
     </div>
   );
