33960c01136f365540d8fe0e36e48596bf50dd61
Daniela <dare0002@stud.kea.dk>
2025-05-23 23:09:04 +0200

    add framer motion

--- src/components/HeroIndex.jsx
+++ src/components/HeroIndex.jsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Button from "./Button";
-// import { motion } from "@motionone/react";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   const handleScroll = () => {
@@ -15,22 +15,22 @@ const Hero = () => {
   return (
     <div className="bg-lightgreen text-green w-full py-6 mb-8">
       <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-center gap-8 relative">
-        {/* <motion.img
+        <motion.img
                     src="/sallysFarKids.svg"
                     alt="Sallys far og to børn illustration"
                     className="w-54 scale-125 max-h-[400px] hidden md:block object-contain mx-auto z-0"
                     initial={{ x: '-100%', opacity: 1 }}
                     animate={{ x: '40%', opacity: 0 }}
-                    transition={{ duration: 1, ease: 'easeOut' }}
-                /> */}
-        {/* <motion.img
+                    transition={{ duration: 1.1, ease: 'easeOut' }}
+                /> 
+        <motion.img
                 src="/girlDog.svg"
                 alt="Pige med en hund illustration"
-                className="absolute w-54 scale-100 max-h-[250px] hidden md:block object-contain mx-auto z-0"
+                className="absolute w-53 scale-100 max-h-[250px] hidden md:block object-contain mx-auto z-0"
                 initial={{ x: '-120%', opacity: 0 }}
                 animate={{ x: 0, opacity: 1 }}
                 transition={{ duration: 1.5, ease: 'easeInOut', delay:0.5 }}
-                /> */}
+                />
 
         <div className="flex flex-col items-center text-center z-10 relative">
           <h1 className="text-4xl md:text-8xl font-bold mb-1">CARLSEN</h1>
@@ -40,13 +40,13 @@ const Hero = () => {
           <Button onClick={handleScroll}>Find materiale nu</Button>
         </div>
 
-        {/* <motion.img
+         <motion.img
                 src="/girlDog.svg"
                 alt="Pige med en hund illustration"
                 className="absolute left-1/2 transform -translate-x-1/2 w-54 scale-100 max-h-[250px] hidden md:block object-contain mx-auto z-0"
                 initial={{ x: 150, opacity: 1 }}
                 animate={{ x: '120%', opacity: 0 }}
-                transition={{ duration: 1 , ease: 'easeInOut' }}
+                transition={{ duration: 0.9, ease: 'easeInOut' }}
                 />
                 <motion.img
                     src="/sallysFarKids.svg"
@@ -54,8 +54,8 @@ const Hero = () => {
                     className="absolute left-1/2 transform -translate-x-1/2 w-54 scale-125 max-h-[400px] hidden md:block object-contain mx-auto z-0"
                     initial={{ x: '40%', opacity: 0 }}
                     animate={{ x: 300, opacity: 1 }}
-                    transition={{ duration: 1.5, ease: 'easeOut', delay: 0.9 }}
-                /> */}
+                    transition={{ duration: 1, ease: 'easeOut', delay: 1.1 }}
+                />
       </div>
     </div>
   );
